591424fa0c06a2c2c5e667a52ebfe76eae549f01
Danny Raetzsch <danny04@gmail.com>
2023-05-05 09:36:14 -0500

    Improve script readability

--- index.js
+++ index.js
@@ -2,33 +2,27 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// Get api key from env var
 const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY;
+
+// Configurable rate limit
 const RATE_LIMIT_RPS = 5;
+const REQUEST_INTERVAL_MS = 1_000 / RATE_LIMIT_RPS;
 
-const processStartTimeMillis = Date.now();
-
-const openseaEventsUrl = "https://api.opensea.io/api/v1/events?" +
-  new URLSearchParams({
-    event_type: "created",
-    limit: 200,
-  }).toString();
-
-function timeout(timeoutMillis) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, timeoutMillis);
-  });
+function getOpenseaEventsUrl(occurredBefore) {
+  return "https://api.opensea.io/api/v1/events?" +
+    new URLSearchParams({
+      event_type: "created",
+      limit: 200,
+      occurred_before: new Date(occurredBefore).toISOString(),
+    }).toString();
 }
 
 async function fetchOpenseaEventsBefore(occurredBefore) {
   console.log(`[${new Date().toISOString()}] Sending Opensea request`);
   const response = await fetch(
-    openseaEventsUrl + "&occurred_before=" +
-      new Date(occurredBefore).toISOString(),
-    {
-      headers: {
-        "X-API-KEY": OPENSEA_API_KEY,
-      },
-    }
+    getOpenseaEventsUrl(occurredBefore),
+    { headers: { "X-API-KEY": OPENSEA_API_KEY } }
   );
 
   const status = response.status;
@@ -39,13 +33,18 @@ async function fetchOpenseaEventsBefore(occurredBefore) {
   }
 }
 
-(async () => {
+async function main() {
   const startTime = Date.now();
 
-  for (let i = 0; i < 1000000; i++) {
-    fetchOpenseaEventsBefore(startTime - i * 10000);
-    await timeout(1000 / RATE_LIMIT_RPS);
+  for (let i = 0; ; i++) {
+    // specify a different occurred_before value for each request
+    // so that each response has a different batch of events
+    fetchOpenseaEventsBefore(startTime - i * 10_000);
+
+    // wait to avoid exceeding rate limit
+    await new Promise((resolve) => { setTimeout(resolve, REQUEST_INTERVAL_MS) });
   }
   process.exit(0);
-})();
+}
+main();
 
